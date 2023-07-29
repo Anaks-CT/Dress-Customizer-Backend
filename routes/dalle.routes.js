24d2e889b3bc275c9ebd0ac665f453b97d5eb4db
Anaks-CT <anaksthayyil30@gmail.com>
2023-07-29 20:57:17 +0530

    finished backend. used unsplash API to generate images using prmpt. ready for deployment

---
+++ routes/dalle.routes.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi} from 'openai';
+import axios from 'axios';
+
+dotenv.config();
+
+const router = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      headers: {
+        Authorization: `Client-ID ${UNSPLASH_API_KEY}`
+      },
+      params: {
+        query: prompt
+      }
+    });
+
+    // Get the image URL from the API response
+    const imageUrl = response.data.results[0].urls.raw;
+
+    // Fetch the image data from the URL
+    const imageResponse = await fetch(imageUrl);
+    const imageBuffer = await imageResponse.arrayBuffer();
+
+    // Convert the image data to base64
+    const imageBase64 = Buffer.from(imageBuffer).toString('base64');
+
+    res.status(200).json({ photo: imageBase64 });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+});
+
+export default router;
